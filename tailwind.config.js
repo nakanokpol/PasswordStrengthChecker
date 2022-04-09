fe531554b45696b8b96b27f5147132aaa88e3281
KUNKANIT THONGKENG <92772306+Imfirn@users.noreply.github.com>
2022-04-09 17:11:39 +0700

    navbar

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        prompt:"'Prompt', sans-serif",
+      }
+    },
+  },
+  plugins: [],
+}
