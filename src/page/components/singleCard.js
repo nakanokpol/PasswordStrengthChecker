55d36d4d8f51e16fe7b72004e47fdec22e59bcd3
nakanokpol <nakanokpol49066@gmail.com>
2022-04-16 22:39:51 +0700

    update Card

--- src/page/components/singleCard.js
+++ src/page/components/singleCard.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const CardTemplate1 = (card_props)=>{
-    const {Number, DrawDate, Draw} = card_props
+const SingleCardTemplate = (card_props)=>{
+    const {Number, DrawDate, Draw, Storename} = card_props
     return (
-        <div style={{height: "8.5VW", width: "17VW"}}>
-            <div className="bg-white shadow-lg shadow-cyan-500/50 items-center font-prompt" style={{height: "7.9VW", width: "17VW", borderWidth:"0.01VW"}}>
+        <div className="font-prompt" style={{height: "8.5VW", width: "17VW"}}>
+            <div className="bg-white items-center" style={{height: "7.9VW", width: "17VW", borderWidth:"0.01VW", boxShadow:"0px 0px 7px 0px #B8D4D4"}}>
                 <div style={{marginLeft: "6.545VW", marginTop: "0.9875VW",height: "0.228VW", width: "8.5VW", backgroundColor: "#FFE5A3"}}></div>
                 <div style={{marginLeft: "6.545VW", marginTop: "0.19VW",height: "0.228VW", width: "8.5VW", backgroundColor: "#FFE5A3"}}></div>
                 <div style={{marginLeft: "6.545VW", marginTop: "0.19VW",height: "0.228VW", width: "8.5VW", backgroundColor: "#FFE5A3"}}></div>
@@ -21,17 +21,31 @@ const CardTemplate1 = (card_props)=>{
                     <h1 className="text-center" style={{fontSize: "0.55VW"}}>ชุดที่</h1>
                 </div>
                 <div style={{marginLeft: "14.055VW",marginTop: "-0.9115VW",height: "0.9115VW", width: "3VW"}}>
-                    <h1 className="text-left font-normal" style={{fontSize: "0.55VW", color:"#ff0000"}}>xx</h1>
+                    <h1 className="text-left font-normal" style={{fontSize: "0.55VW"}}>-</h1>
                 </div>
                 <div style={{marginLeft: "6.545VW", marginTop: "-5.89VW",height: "1.482VW", width: "8.5VW"}}>
-                    <h1 className="text-center font-normal" style={{fontSize: "1.2VW", }}>{Number}</h1>
+                    <h1 className="text-center font-normal" style={{fontSize: "1.2VW", letterSpacing:"0.35VW"}}>{Number}</h1>
                 </div>
                 <div style={{marginLeft: "6.545VW", marginTop: "0.9VW",height: "1.482VW", width: "8.5VW"}}>
                     <h1 className="text-center font-light" style={{fontSize: "0.8VW"}}>{DrawDate}</h1>
+                </div>
+            </div>
+            <div style={{height: "3.4VW", width: "17VW"}}>
+                <div style={{marginTop: "0.3798VW", height: "1.1394VW", width: "4.3822VW",borderRadius: "1VW", backgroundColor:"#D3FAFA"}}>
+                    <h1 className="text-center" style={{fontSize: "0.7VW"}}>สลากเดี่ยว</h1>
+                </div>
+                <div style={{marginTop: "0.3VW", height: "1.5VW", width: "11VW"}}>
+                    <h1 className="text-left" style={{fontSize: "0.9VW"}}>{Storename}</h1>
+                </div>
+                <div style={{marginLeft: "12VW",marginTop: "-2.2VW", height: "2.25VW", width: "2.5VW"}}>
+                    <h1 className="text-left" style={{fontSize: "1.5VW"}}>80</h1>
+                </div>
+                <div style={{marginLeft: "14.5VW",marginTop: "-1.5VW", height: "1.5VW", width: "1.5VW"}}>
+                    <h1 className="text-left font-light" style={{fontSize: "0.8VW"}}>บาท</h1>
                 </div>
             </div>
         </div>
     );
 }
 
-export default CardTemplate1
+export default SingleCardTemplate
--- src/page/home.js
+++ src/page/home.js
@@ -1,36 +1,36 @@
 import React from "react";
-import CardTemplate1 from "./components/card"
-
-// const li_data = [
-//   {Number:"000000", DrawDate:"1 เมษายน 2565", Draw: "20", Storename: "สำรีขายหวย"},
-//   {Number:"123456", DrawDate:"16 เมษายน 2565", Draw: "10", Storename: "สำรีขายหวย"},
-//   {Number:"878461", DrawDate:"1 เมษายน 2565", Draw: "2", Storename: "หวยล้นฟ้า"},
-//   {Number:"484461", DrawDate:"1 เมษายน 2565", Draw: "1", Storename: "สำรีขายหวย"},
-//   {Number:"090545", DrawDate:"1 เมษายน 2565", Draw: "15", Storename: "แมวตัวโปรด"},
-//   {Number:"140154", DrawDate:"1 เมษายน 2565", Draw: "17", Storename: "โลกที่ไม่มีเธอ"},
-//   {Number:"090145", DrawDate:"16 เมษายน 2565", Draw: "33", Storename: "สำรีขายหวย"},
-//   {Number:"999997", DrawDate:"1 เมษายน 2565", Draw: "27", Storename: "คนไม่จำเป็น"},
-//   {Number:"774164", DrawDate:"1 เมษายน 2565", Draw: "13", Storename: "ประยวย"}
-// ]
+import SingleCardTemplate from "./components/singleCard"
+import PackedCardTemplate from "./components/packedCard"
+import SingleCardTemplateTest from "./components/singleCardTemplateTest"
+import PackedCardTemplateTest from "./components/packedCardTemplateTest"
 
 const li_data = [
-  {Number:"000000", DrawDate:"1 เมษายน 2565", Draw: "20"},
-  {Number:"123456", DrawDate:"16 เมษายน 2565", Draw: "10"},
-  {Number:"878461", DrawDate:"1 เมษายน 2565", Draw: "2"},
-  {Number:"484461", DrawDate:"1 เมษายน 2565", Draw: "1"},
-  {Number:"090545", DrawDate:"1 เมษายน 2565", Draw: "15"},
-  {Number:"140154", DrawDate:"1 เมษายน 2565", Draw: "17"},
-  {Number:"090145", DrawDate:"16 เมษายน 2565", Draw: "33"},
-  {Number:"999997", DrawDate:"1 เมษายน 2565", Draw: "27"},
-  {Number:"774164", DrawDate:"1 เมษายน 2565", Draw: "13"}
+  {Number:"000000", DrawDate:"1 เมษายน 2565", Draw: "20", Storename: "สำรีขายหวย", pack:"N"},
+  {Number:"123456", DrawDate:"16 เมษายน 2565", Draw: "10", Storename: "แมวน้ำตัวเล็ก", pack:"Y"},
+  {Number:"878461", DrawDate:"1 เมษายน 2565", Draw: "2", Storename: "สำรีขายไก่", pack:"N"},
+  {Number:"484461", DrawDate:"1 เมษายน 2565", Draw: "1", Storename: "ทำไมทำไม", pack:"N"},
+  {Number:"090545", DrawDate:"1 เมษายน 2565", Draw: "15", Storename: "ไข่เป็นแฟนได้หมอ", pack:"Y"},
+  {Number:"140154", DrawDate:"1 เมษายน 2565", Draw: "17", Storename: "เป็นลูกครึ่งนิดนึง", pack:"Y"},
+  {Number:"090145", DrawDate:"16 เมษายน 2565", Draw: "33", Storename: "หยุดโดยไม่มีอะไรกั้น", pack:"Y"},
+  {Number:"999997", DrawDate:"1 เมษายน 2565", Draw: "27", Storename: "แลคตาซอยห้าบาท", pack:"N"},
+  {Number:"774164", DrawDate:"1 เมษายน 2565", Draw: "13", Storename: "ขอบคุณแต่คราวหลังไม่ต้อง", pack:"N"},
+  {Number:"851945", DrawDate:"1 เมษายน 2565", Draw: "15", Storename: "ฉันคือประธานบริษัท", pack:"N"},
+  {Number:"156174", DrawDate:"1 เมษายน 2565", Draw: "17", Storename: "สำรวยขายหวี", pack:"N"},
+  {Number:"784166", DrawDate:"16 เมษายน 2565", Draw: "33", Storename: "แมวตัวโปรด", pack:"Y"},
+  {Number:"196135", DrawDate:"1 เมษายน 2565", Draw: "27", Storename: "ไข่เป็นคนเอ้าจอ", pack:"N"},
+  {Number:"151512", DrawDate:"1 เมษายน 2565", Draw: "13", Storename: "หวยชี้ฟ้า", pack:"N"}
 ]
 
 function Home() {
   return (
     <div className="h-screen bg-white" style={{margin:"2% 5% 5% 5%"}}>
-      <div className="h-screen grid grid-rows-5 grid-cols-5 justify-items-center">
+      <div className="h-screen grid grid-cols-1 2xl:grid-cols-5 xl:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 justify-items-center" style={{gridTemplateRows: "repeat(auto-fill, 11.5VW)"}}>
         {li_data.map((element)=>{
-            return <CardTemplate1 Number={element.Number} DrawDate={element.DrawDate} Draw={element.Draw}/>
+          if (element.pack==="N") {
+            return <SingleCardTemplate Number={element.Number} DrawDate={element.DrawDate} Draw={element.Draw} Storename={element.Storename}/>
+          }else{
+            return <PackedCardTemplate Number={element.Number} DrawDate={element.DrawDate} Draw={element.Draw} Storename={element.Storename}/>
+          }
         })}
       </div>
     </div>
