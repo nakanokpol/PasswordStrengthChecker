4a5c1a66507abbbacfebf4f1be2a38e0a22c1452
nakanokpol <nakanokpol49066@gmail.com>
2022-04-16 17:00:55 +0700

    update card

--- src/page/components/card.js
+++ src/page/components/card.js
@@ -1,15 +1,37 @@
 import React from "react";
 
-const CardTemplate = ()=>{
+const CardTemplate1 = (card_props)=>{
+    const {Number, DrawDate, Draw} = card_props
     return (
-        <div className="bg-white shadow-lg shadow-cyan-500/50" style={{height: "7.9VW", width: "17VW", borderWidth:"0.1VW"}}>
-            <div>
-                <h1 className="grow-0">
-                    POL
-                </h1>
+        <div style={{height: "8.5VW", width: "17VW"}}>
+            <div className="bg-white shadow-lg shadow-cyan-500/50 items-center font-prompt" style={{height: "7.9VW", width: "17VW", borderWidth:"0.01VW"}}>
+                <div style={{marginLeft: "6.545VW", marginTop: "0.9875VW",height: "0.228VW", width: "8.5VW", backgroundColor: "#FFE5A3"}}></div>
+                <div style={{marginLeft: "6.545VW", marginTop: "0.19VW",height: "0.228VW", width: "8.5VW", backgroundColor: "#FFE5A3"}}></div>
+                <div style={{marginLeft: "6.545VW", marginTop: "0.19VW",height: "0.228VW", width: "8.5VW", backgroundColor: "#FFE5A3"}}></div>
+                <div style={{marginLeft: "6.545VW", marginTop: "0.19VW",height: "0.228VW", width: "8.5VW", backgroundColor: "#FFE5A3"}}></div>
+                <div style={{marginLeft: "15.57VW", marginTop: "-2.4695VW",height: "7.8588VW", width: "1.35VW", backgroundColor: "#D3FAFA"}}></div>
+                <div style={{marginLeft: "1.2784VW", marginTop: "-6.8788VW",height: "4.7095VW", width: "4.7288VW", backgroundColor: "#C4C4C4"}}></div>
+                <div style={{marginLeft: "6.545VW", marginTop: "0.19VW",height: "0.9115VW", width: "1.8VW", backgroundColor: "#D3FAFA"}}>
+                    <h1 className="text-center" style={{fontSize: "0.55VW"}}>งวดที่</h1>
+                </div>
+                <div style={{marginLeft: "8.6VW",marginTop: "-0.9115VW",height: "0.9115VW", width: "3VW"}}>
+                    <h1 className="text-left font-normal" style={{fontSize: "0.55VW"}}>{Draw}</h1>
+                </div>
+                <div style={{marginLeft: "12VW", marginTop: "-0.9115VW",height: "0.9115VW", width: "1.8VW", backgroundColor: "#D3FAFA"}}>
+                    <h1 className="text-center" style={{fontSize: "0.55VW"}}>ชุดที่</h1>
+                </div>
+                <div style={{marginLeft: "14.055VW",marginTop: "-0.9115VW",height: "0.9115VW", width: "3VW"}}>
+                    <h1 className="text-left font-normal" style={{fontSize: "0.55VW", color:"#ff0000"}}>xx</h1>
+                </div>
+                <div style={{marginLeft: "6.545VW", marginTop: "-5.89VW",height: "1.482VW", width: "8.5VW"}}>
+                    <h1 className="text-center font-normal" style={{fontSize: "1.2VW", }}>{Number}</h1>
+                </div>
+                <div style={{marginLeft: "6.545VW", marginTop: "0.9VW",height: "1.482VW", width: "8.5VW"}}>
+                    <h1 className="text-center font-light" style={{fontSize: "0.8VW"}}>{DrawDate}</h1>
+                </div>
             </div>
         </div>
     );
 }
 
-export default CardTemplate
+export default CardTemplate1
--- src/page/home.js
+++ src/page/home.js
@@ -1,14 +1,38 @@
 import React from "react";
-import CardTemplate from "./components/card"
+import CardTemplate1 from "./components/card"
+
+// const li_data = [
+//   {Number:"000000", DrawDate:"1 เมษายน 2565", Draw: "20", Storename: "สำรีขายหวย"},
+//   {Number:"123456", DrawDate:"16 เมษายน 2565", Draw: "10", Storename: "สำรีขายหวย"},
+//   {Number:"878461", DrawDate:"1 เมษายน 2565", Draw: "2", Storename: "หวยล้นฟ้า"},
+//   {Number:"484461", DrawDate:"1 เมษายน 2565", Draw: "1", Storename: "สำรีขายหวย"},
+//   {Number:"090545", DrawDate:"1 เมษายน 2565", Draw: "15", Storename: "แมวตัวโปรด"},
+//   {Number:"140154", DrawDate:"1 เมษายน 2565", Draw: "17", Storename: "โลกที่ไม่มีเธอ"},
+//   {Number:"090145", DrawDate:"16 เมษายน 2565", Draw: "33", Storename: "สำรีขายหวย"},
+//   {Number:"999997", DrawDate:"1 เมษายน 2565", Draw: "27", Storename: "คนไม่จำเป็น"},
+//   {Number:"774164", DrawDate:"1 เมษายน 2565", Draw: "13", Storename: "ประยวย"}
+// ]
+
+const li_data = [
+  {Number:"000000", DrawDate:"1 เมษายน 2565", Draw: "20"},
+  {Number:"123456", DrawDate:"16 เมษายน 2565", Draw: "10"},
+  {Number:"878461", DrawDate:"1 เมษายน 2565", Draw: "2"},
+  {Number:"484461", DrawDate:"1 เมษายน 2565", Draw: "1"},
+  {Number:"090545", DrawDate:"1 เมษายน 2565", Draw: "15"},
+  {Number:"140154", DrawDate:"1 เมษายน 2565", Draw: "17"},
+  {Number:"090145", DrawDate:"16 เมษายน 2565", Draw: "33"},
+  {Number:"999997", DrawDate:"1 เมษายน 2565", Draw: "27"},
+  {Number:"774164", DrawDate:"1 เมษายน 2565", Draw: "13"}
+]
+
 function Home() {
   return (
-    <div className="h-screen flex justify-evenly items-center bg-white" style={{marginLeft: "2%",marginRight: "2%"}}>
-      <CardTemplate/>
-      <CardTemplate/>
-      <CardTemplate/>
-      <CardTemplate/>
-      <CardTemplate/>
-      
+    <div className="h-screen bg-white" style={{margin:"2% 5% 5% 5%"}}>
+      <div className="h-screen grid grid-rows-5 grid-cols-5 justify-items-center">
+        {li_data.map((element)=>{
+            return <CardTemplate1 Number={element.Number} DrawDate={element.DrawDate} Draw={element.Draw}/>
+        })}
+      </div>
     </div>
   );
 }
